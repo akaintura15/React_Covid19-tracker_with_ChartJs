5d4df45d3bedda1a4936b1b3a11bd82cc718dc61
akaintura15 <akaintura15@gmail.com>
2020-12-06 01:09:47 +0530

    Updates

---
+++ precache-manifest.087d0e3a337b4072d9b08e1d3fc3bfbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc05cbf44ccbd1025a8ab3cc1b60a331",
+    "url": "/React_Covid19-tracker_with_ChartJs/index.html"
+  },
+  {
+    "revision": "4e7e3aa76ec3eb6526ed",
+    "url": "/React_Covid19-tracker_with_ChartJs/static/css/main.7b6c44fd.chunk.css"
+  },
+  {
+    "revision": "8f7936ad18b38dd84742",
+    "url": "/React_Covid19-tracker_with_ChartJs/static/js/2.af1892be.chunk.js"
+  },
+  {
+    "revision": "639c28c8ef14bcd37d017fe8f6dc04e2",
+    "url": "/React_Covid19-tracker_with_ChartJs/static/js/2.af1892be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7e3aa76ec3eb6526ed",
+    "url": "/React_Covid19-tracker_with_ChartJs/static/js/main.6d1da88e.chunk.js"
+  },
+  {
+    "revision": "ecf279031bdfb137f7a5",
+    "url": "/React_Covid19-tracker_with_ChartJs/static/js/runtime-main.f5c6d25a.js"
+  },
+  {
+    "revision": "b3591a3b973c0e0919d0342155d17322",
+    "url": "/React_Covid19-tracker_with_ChartJs/static/media/c.b3591a3b.jpg"
+  }
+]);
